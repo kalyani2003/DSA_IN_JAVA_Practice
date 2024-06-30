e78688efbcea854f22f98f80b50755eb17c2c6f7
KALYANI SANJAY MHALA <84217977+kalyani2003@users.noreply.github.com>
2024-06-30 23:07:12 +0530

    Add files via upload

---
+++ Patterns Problems/halfrectangle2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class halfrectangle2 {
+    public static void main(String[] args) {
+        int n =10;
+        for(int i=n; i<=n; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
